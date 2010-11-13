ca57a7903cc51aeae666b12f7c672395a6b818e5
Pepijn Looije <pepijnlooije@gmail.com>
2010-11-13 18:00:28 +0100

    Longuitleg toegevoegd

--- lib/lichaam/onderdelen/organen/longen.rb
+++ lib/lichaam/onderdelen/organen/longen.rb
@@ -1,6 +1,9 @@
 module Lichaam
   module Onderdelen
     module Organen
+      # "Vanuit de lucht in de longblaasjes lost zuurstof eerst op in het alveolaire vocht; van daaruit vindt diffusie plaats naar het bloed in de longhaarvaten. Deze diffusie wordt vooral veroorzaakt door het verschil in zuurfstofspanning (pO2) tussen het alveolaire vocht en het bloedplasma. Dit verschil wordt gehandhaafd doordat de alveolaire lucht voortdurend wordt ververst en doordat het bloed in de longblaasjes stroomt."
+      # "In de longhaarvaten bevindt koolstofdioxide zich o.a. opgelost in het bloedplasma. Door het verschil in koolstofdioxidespanning (pCO2) vindt diffusie plaats van het bloedplasma naar het alveolaire vocht; van daarut wordt koolstofdioxide afgegeven aan de alveolaire lucht."
+      # Bron: Biologie voor Jou (ISBN: 978 90 208 7390 0)
       class Longen < Orgaan
       end
     end
