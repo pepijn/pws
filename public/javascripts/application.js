e586a32ec480c1305a02d6299406df5d6ff43b45
Pepijn Looije <pepijnlooije@gmail.com>
2010-11-15 23:06:12 +0100

    Piechart ter visualisatie van de bloeddruk toegevoegd

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,28 @@
 $(function() {
+	// Creates canvas 640 × 480 at 10, 50
+	var r = Raphael(10, 50, 640, 480);
+	// Creates pie chart at with center at 320, 200,
+	// radius 100 and data: [55, 20, 13, 32, 5, 1, 2]
+
+	var bloeddruk_chart, oxihemoglobinen_chart;
+
   var ws = new WebSocket("ws://localhost:8080");
   ws.onmessage = function(evt) {
     $("#debug").html(evt.data);
 
+		var legend = [];
+		var bloeddruk = [];
+		var oxihemoglobinen = [];
+
 		$.each(JSON.parse(evt.data), function(naam, data) {
-			var onderdeel = onderdelen[naam];
-			var verhouding = data.bloed.oxihemoglobinen * 0.5;
-			if(onderdeel) {
-				onderdeel.attr("fill", "rgb(" + verhouding + ",0," + (255 - verhouding) + ")");
-				onderdeel.attr("fill-opacity", data.bloed.druk / 100);
-			}
+			legend.push(naam);
+			bloeddruk.push(data.bloed.druk);
+			oxihemoglobinen.push(data.bloed.oxihemoglobinen > 0 ?  data.bloed.oxihemoglobinen : 40);
 		});
+
+		r.clear();
+		bloeddruk_chart = r.g.piechart(300, 300, 150, bloeddruk, { legend: legend });
+		// oxihemoglobinen_chart = r.g.piechart(400, 240, 100, oxihemoglobinen, { legend: legend });
   };
 
   ws.onclose = function() { $("#debug").html("Verbinding gesloten") };
@@ -29,14 +41,14 @@ $(function() {
 	}
 
 	function hartslag() {
-		ws.send("boezemsystole");
-		pomp(onderdelen["Linkerboezem"], 0.80);
-		pomp(onderdelen["Rechterboezem"], 0.80);
+		ws.send("boezemsystole");		//
+				// pomp(onderdelen["Linkerboezem"], 0.80);
+				// pomp(onderdelen["Rechterboezem"], 0.80);
 
 		setTimeout(function() {
-		  ws.send("kamersystole");
-			pomp(onderdelen["Linkerkamer"], 0.60);
-			pomp(onderdelen["Rechterkamer"], 0.60);
+		  ws.send("kamersystole");			//
+		  			// pomp(onderdelen["Linkerkamer"], 0.60);
+		  			// pomp(onderdelen["Rechterkamer"], 0.60);
 		}, 100);
 
 		setTimeout(function() {
